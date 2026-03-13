f3b135af2fdb2082077e50d0336ca50399ad2721
yuyang.robert <yuyang.robert@bytedance.com>
2026-03-13 11:18:04 +0800

    Initial Commit

---
+++ MyTraeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTraeProject
+//
+//  Created by ByteDance on 2026/3/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
